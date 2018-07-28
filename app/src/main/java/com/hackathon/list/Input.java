e3c271d7ed901fcfa74262f3b5853a85bd32f045
Joelinjatovo <joelinjatovo@gmail.com>
2018-07-28 15:07:05 +0300

    Home Button View

---
+++ app/src/main/java/com/hackathon/list/Input.java
@@ -0,0 +1,13 @@
+package com.hackathon.list;
+
+public class Input {
+    private String CIN;
+
+    public String getCIN() {
+        return CIN;
+    }
+
+    public void setCIN(String CIN) {
+        this.CIN = CIN;
+    }
+}
