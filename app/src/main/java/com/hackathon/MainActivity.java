ddd8cbd25f9c7a64918f98ce67456bb09d7304c6
Joelinjatovo <joelinjatovo@gmail.com>
2018-07-29 03:08:22 +0300

    Video Not working

--- app/src/main/java/com/hackathon/MainActivity.java
+++ app/src/main/java/com/hackathon/MainActivity.java
@@ -1,41 +1,54 @@
 package com.hackathon;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Toast;
 
 import com.hackathon.candidate.CandidateGridActivity;
-import com.hackathon.candidate.CandidateListActivity;
 import com.hackathon.election.ElectionListActivity;
 import com.hackathon.video.VideoGridActivity;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private View btn_doleance;
     private View btn_candidat;
     private View btn_election;
-    private View btn_sondage;
     private View btn_video;
+    private View btn_file;
+    private View btn_list;
+    private View btn_setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadView();
+
+    }
+
+    private void loadView() {
         setContentView(R.layout.activity_main);
 
-        btn_doleance = findViewById(R.id.btn_doleance);
         btn_candidat = findViewById(R.id.btn_candidat);
-        btn_election = findViewById(R.id.btn_election);
-        btn_sondage = findViewById(R.id.btn_sondage);
         btn_video = findViewById(R.id.btn_video);
+        btn_file = findViewById(R.id.btn_file);
+        btn_election = findViewById(R.id.btn_election);
+        btn_list = findViewById(R.id.btn_list);
+        btn_setting = findViewById(R.id.btn_setting);
 
-        btn_doleance.setOnClickListener(this);
         btn_candidat.setOnClickListener(this);
-        btn_election.setOnClickListener(this);
-        btn_sondage.setOnClickListener(this);
         btn_video.setOnClickListener(this);
-
+        btn_file.setOnClickListener(this);
+        btn_election.setOnClickListener(this);
+        btn_list.setOnClickListener(this);
+        btn_setting.setOnClickListener(this);
     }
 
     @Override
@@ -51,18 +64,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 i = new Intent(this, ElectionListActivity.class);
                 startActivity(i);
                 break;
-            case R.id.btn_doleance :
-                i = new Intent(this, LoginActivity.class);
-                startActivity(i);
-                break;
-            case R.id.btn_sondage :
-                i = new Intent(this, LoginActivity.class);
-                startActivity(i);
-                break;
             case R.id.btn_video :
                 i = new Intent(this, VideoGridActivity.class);
                 startActivity(i);
                 break;
+            case R.id.btn_file :
+            case R.id.btn_list :
+                Toast.makeText(this, "En cours de developpement", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_setting :
+                showDialogBox();
+                break;
         }
+    }
+
+    private void showDialogBox() {
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.btn_parametre))
+            .setMessage("Are you sure you want to delete this entry?")
+            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // continue with delete
+                    setLocale("mg");
+                }
+            })
+            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // do nothing
+                }
+            })
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show();
+    }
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+
+        onConfigurationChanged(conf);
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        loadView();
     }
 }
